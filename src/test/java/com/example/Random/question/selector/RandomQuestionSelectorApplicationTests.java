1d5cf7a835b7af8eebf16d4d07fd1bde668389e4
UPADHYAY <abhishekupadhyay260@gmail.com>
2023-06-27 20:52:29 +0530

    first commit

---
+++ src/test/java/com/example/Random/question/selector/RandomQuestionSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Random.question.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQuestionSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
